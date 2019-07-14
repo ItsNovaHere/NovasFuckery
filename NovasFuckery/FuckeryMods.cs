1a19fb6235b30f50211b835b7f70a79760910faf
ItsNovaHere <theawesome624@hotmail.com>
2019-07-13 21:33:16 -0500

    PogU

--- NovasFuckery/FuckeryMods.cs
+++ NovasFuckery/FuckeryMods.cs
@@ -5,34 +5,27 @@ using NovasFuckery.Util;
 using System.Linq;
 using NovasFuckery.Mods;
 using UnityEngine;
-using Object = UnityEngine.Object;
-using UnityEngine.XR;
+using Random = UnityEngine.Random;
 
 namespace NovasFuckery
 {
 
     internal static class FuckeryMods {
-        internal static BeatmapObjectSpawnController SpawnController;
-        internal static StandardLevelGameplayManager PauseManager;
-        internal static SaberManager SaberManager;
+        
 
         internal static void SetupMods () {
             bool disableScore = false;
 
             BS_Utils.Plugin.LevelDidFinishEvent += Plugin_LevelDidFinishEvent;
 
-            SpawnController = Resources.FindObjectsOfTypeAll<BeatmapObjectSpawnController>().First();
-            SpawnController.noteWasMissedEvent += FuckeryMods_noteWasMissedEvent;
-
-            PauseManager = Resources.FindObjectsOfTypeAll<StandardLevelGameplayManager>().First();
-            SaberManager = Resources.FindObjectsOfTypeAll<SaberManager>().First();
+            MissHell.SetupHell();
 
             if (FuckeryUI.OneAtATime.Enabled) 
                 OAAT();
 
             if (FuckeryUI.InvisibleSabers.Enabled)
                 InvisbleSabers();
-
+             
             if (FuckeryUI.ModsEnabled()) {
                 disableScore = true;
             }
@@ -43,26 +36,13 @@ namespace NovasFuckery
         }
 
         private static void Plugin_LevelDidFinishEvent(StandardLevelScenesTransitionSetupDataSO levelScenesTransitionSetupDataSO, LevelCompletionResults levelCompletionResults) {
-            SpawnController.noteWasMissedEvent -= FuckeryMods_noteWasMissedEvent;
-            SpawnController = null;
+            try {
+                MissHell.ResetHell();
+            } catch {
 
-            PauseManager = null;
-            SaberManager = null;
-
-            BS_Utils.Plugin.LevelDidFinishEvent -= Plugin_LevelDidFinishEvent;
-        }
-
-        private static void FuckeryMods_noteWasMissedEvent(BeatmapObjectSpawnController arg1, NoteController arg2) {
-            if (FuckeryUI.PauseOnMiss.Enabled) {
-                if (PauseManager) {
-                    PauseManager.Pause();
-                }
-            }
-
-            if (FuckeryUI.AYYYYYOnMiss.Enabled) {
-                Plugin.MissSounds[UnityEngine.Random.Range(0, Plugin.MissSounds.Count - 1)].Play();
             }
         }
+
 
         private static void InvisbleSabers () {
             Resources.FindObjectsOfTypeAll<PlayerController>().First().gameObject.AddComponent<InvisibleSabers>();
---
+++ NovasFuckery/MissHell.cs
@@ -0,0 +1,80 @@
+﻿using BS_Utils.Utilities;
+using NovasFuckery.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace NovasFuckery
+{
+    internal static class MissHell
+    {
+        internal static BeatmapObjectSpawnController SpawnController;
+        internal static StandardLevelGameplayManager PauseManager;
+        internal static PlayerController PlayerController;
+        internal static SaberManager SaberManager;
+
+        internal static Vector3 StartPosition;
+
+        internal static void SetupHell() {
+            SpawnController = Resources.FindObjectsOfTypeAll<BeatmapObjectSpawnController>().FirstOrDefault();
+            PauseManager = Resources.FindObjectsOfTypeAll<StandardLevelGameplayManager>().FirstOrDefault();
+            PlayerController = Resources.FindObjectsOfTypeAll<PlayerController>().FirstOrDefault();
+
+            SaberManager = PlayerController.GetField("_saberManager") as SaberManager;
+
+            if (SpawnController) {
+                SpawnController.noteWasMissedEvent += MissHell_noteWasMissedEvent;
+                SpawnController.noteWasCutEvent += MissHell_noteWasCutEvent;
+            }
+
+            StartPosition = PlayerController.transform.position;
+        }
+
+        internal static void ResetHell() {
+            SpawnController.noteWasMissedEvent -= MissHell_noteWasMissedEvent;
+            SpawnController.noteWasCutEvent -= MissHell_noteWasCutEvent;
+            SpawnController = null;
+
+            PauseManager = null;
+            SaberManager = null;
+
+            PlayerController.transform.position = StartPosition;
+        }
+
+        internal static void NoteMissed() {
+            if (FuckeryUI.PauseOnMiss.Enabled) {
+                if (PauseManager) {
+                    PauseManager.Pause();
+                }
+            }
+
+            if (FuckeryUI.AYYYYYOnMiss.Enabled) {
+                Plugin.MissSounds[Random.Range(0, Plugin.MissSounds.Count - 1)].Play();
+            }
+
+            if (FuckeryUI.RoomAdjustOnMiss.Enabled) {
+                var x = Random.Range(0, 10);
+                var y = Random.Range(0, 10);
+                var z = Random.Range(0, 10);
+
+                PlayerController.gameObject.transform.Translate(new Vector3(x * 0.1f, y * 0.1f, z * 0.1f));
+            }
+        }
+
+        private static void MissHell_noteWasCutEvent(BeatmapObjectSpawnController arg1, NoteController arg2, NoteCutInfo arg3) {
+            if (!arg3.allIsOK) {
+                NoteMissed();
+            }
+        }
+
+        private static void MissHell_noteWasMissedEvent(BeatmapObjectSpawnController arg1, NoteController arg2) {
+            if (arg2.noteData.noteType != NoteType.Bomb) {
+                NoteMissed();
+            }
+        }
+    }
+}
